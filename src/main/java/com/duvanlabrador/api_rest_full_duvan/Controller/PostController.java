3805312638292f3bd698f88ca86837935ec215b3
Duvan Labrador <carlosduvanlh@ufps.edu.co>
2023-07-12 17:10:32 -0500

    Api rest CRUD

---
+++ src/main/java/com/duvanlabrador/api_rest_full_duvan/Controller/PostController.java
@@ -0,0 +1,67 @@
+package com.duvanlabrador.api_rest_full_duvan.Controller;
+
+import com.duvanlabrador.api_rest_full_duvan.DTO.PostDTO;
+import com.duvanlabrador.api_rest_full_duvan.Service.PostService;
+import com.duvanlabrador.api_rest_full_duvan.Service.PostServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/post")
+public class PostController {
+    private final PostService postService;
+    @Autowired
+    public PostController(PostService postService){
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDTO> savePost(@RequestBody PostDTO postDTO){
+
+        try{
+            PostDTO post = this.postService.createPost(postDTO);
+            return new ResponseEntity<PostDTO>(post,HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<PostDTO>(HttpStatus.CONFLICT);
+        }
+    }
+    @GetMapping
+    public List<PostDTO> getAllPost(){
+        return this.postService.getAllPosts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> getPostForId(@PathVariable Long id){
+        try{
+            PostDTO post = this.postService.getPostForId(id);
+            return new ResponseEntity<PostDTO>(post, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<PostDTO>(HttpStatus.CONFLICT);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDTO> updatePost(@RequestBody PostDTO postDTO, @PathVariable Long id){
+        try{
+            PostDTO post = postService.updatePost(postDTO,id);
+            return new ResponseEntity<PostDTO>(post,HttpStatus.ACCEPTED);
+        }catch (Exception e){
+            return new ResponseEntity<PostDTO>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable Long id){
+        try {
+            postService.deletePost(id);
+            return new ResponseEntity<>("Post Delete Correctly", HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+}
